c7cf2c0343a60a8c426e0f2cc7d4163cc5707a8e
Robert Gathanua <robertgathanua@gamail.com>
2024-05-04 09:47:52 +0300

    complete lab

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,17 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe('isPalindrome', () => {
+    it('should return true for palindromic words', () => {
+      expect(isPalindrome("racecar")).toBe(true);
+      expect(isPalindrome("mom")).toBe(true);
+      expect(isPalindrome("madam")).toBe(true);
+    });
+  
+    it('should return false for non-palindromic words', () => {
+      expect(isPalindrome("car")).toBe(false);
+      expect(isPalindrome("hello")).toBe(false);
+    });
+  
+    // Additional test cases can be added here
+  });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,7 @@
 // Your code here
+export function isPalindrome(word) {
+    // Reverse the word and compare with the original
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+  }
+  
